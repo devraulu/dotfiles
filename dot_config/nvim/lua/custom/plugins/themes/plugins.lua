8d111fa9fcb1d3991a8c559634621b6458cacf36
Raul Luis <soyrauluis@gmail.com>
2024-03-30 00:58:57 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/plugins/theme.lua Update .config/nvim/lua/custom/plugins/themes/plugins.lua Update .zshrc

--- dot_config/nvim/lua/custom/plugins/themes/plugins.lua
+++ dot_config/nvim/lua/custom/plugins/themes/plugins.lua
@@ -1,45 +1,58 @@
 return {
-  -- { 'ellisonleao/gruvbox.nvim' },
   --
-  -- { 'catppuccin/nvim' },
-  -- { 'dracula/vim', as = 'dracula' },
-  -- { 'projekt0n/github-nvim-theme' },
-  -- { 'marko-cerovac/material.nvim' },
-  -- { 'loctvl842/monokai-pro.nvim' },
-  -- { 'edeneast/nightfox.nvim' },
-  -- { 'navarasu/onedark.nvim' },
-  -- { 'folke/tokyonight.nvim' },
-  -- { 'catppuccin/nvim' },
-  -- { 'mofiqul/dracula.nvim' },
-  -- { 'projekt0n/github-nvim-theme' },
-  -- { 'ellisonleao/gruvbox.nvim' },
-  --
-  -- { 'marko-cerovac/material.nvim' },
-  -- { 'loctvl842/monokai-pro.nvim' },
-  -- { 'edeneast/nightfox.nvim' },
-  -- { 'navarasu/onedark.nvim' },
-  -- { 'folke/tokyonight.nvim' },
-  -- {
-  --   'talha-akram/noctis.nvim',
-  --   config = function()
-  --     -- vim.cmd [[set background=light]]
-  --     -- vim.cmd.colorscheme 'noctis'
-  --   end,
-  -- },
+  -- --
+  -- -- { 'catppuccin/nvim' },
+  -- -- { 'dracula/vim', as = 'dracula' },
+  -- -- { 'projekt0n/github-nvim-theme' },
+  -- -- { 'marko-cerovac/material.nvim' },
+  -- -- { 'loctvl842/monokai-pro.nvim' },
+  -- -- { 'edeneast/nightfox.nvim' },
+  -- -- { 'navarasu/onedark.nvim' },
   {
-    'ribru17/bamboo.nvim',
-    -- config = function()
-    --   local bamboo = require 'bamboo'
-    --   bamboo.setup {
-    --     term_colors = true,
-    --     transparent = true,
-    --   }
-    --   bamboo.load()
-    --   -- vim.cmd.colorscheme 'bamboo'
-    -- end,
+    'romainl/Apprentice',
+    config = function()
+      vim.cmd [[colorscheme apprentice]]
+    end,
   },
   -- {
-  -- --   'hardhackerlabs/theme-vim',
+  --   'folke/tokyonight.nvim',
+  --   as = 'tokyonight',
+  --   config = function()
+  --     require('tokyonight').setup { transparent = true, style = 'moon', light_style = 'night', comments = { italic = false }, lualine_bold = true }
+  --     vim.cmd [[colorscheme tokyonight]]
+  --   end,
+  -- },
+  -- -- { 'catppuccin/nvim' },
+  -- -- { 'mofiqul/dracula.nvim' },
+  -- -- { 'projekt0n/github-nvim-theme' },
+  -- -- { 'ellisonleao/gruvbox.nvim' },
+  -- --
+  -- -- { 'marko-cerovac/material.nvim' },
+  -- -- { 'loctvl842/monokai-pro.nvim' },
+  -- -- { 'edeneast/nightfox.nvim' },
+  -- -- { 'navarasu/onedark.nvim' },
+  -- -- { 'folke/tokyonight.nvim' },
+  -- -- {
+  -- --   'talha-akram/noctis.nvim',
+  -- --   config = function()
+  -- --     -- vim.cmd [[set background=light]]
+  -- --     -- vim.cmd.colorscheme 'noctis'
+  -- --   end,
+  -- -- },
+  -- {
+  --   'ribru17/bamboo.nvim',
+  --   -- config = function()
+  --   --   local bamboo = require 'bamboo'
+  --   --   bamboo.setup {
+  --   --     term_colors = true,
+  --   --     transparent = true,
+  --   --   }
+  --   --   bamboo.load()
+  --   --   -- vim.cmd.colorscheme 'bamboo'
+  --   -- end,
+  -- },
+  -- {
+  --   'hardhackerlabs/theme-vim',
   --   name = 'hardhacker',
   --   lazy = false,
   --   priority = 1000,
@@ -55,18 +68,18 @@ return {
   -- {
   --   'jaredgorski/SpaceCamp',
   --   config = function()
-  --     -- vim.cmd.colorscheme 'SpaceCamp'
+  --     vim.cmd.colorscheme 'SpaceCamp'
   --   end,
   -- },
-  {
-    'sainnhe/sonokai',
-    config = function()
-      vim.g.sonokai_style = 'shusia'
-      vim.g.sonokai_better_performance = 1
-      vim.g.sonokai_transparent_background = 2
-      vim.cmd.colorscheme 'sonokai'
-    end,
-  },
+  -- {
+  --   'sainnhe/sonokai',
+  --   config = function()
+  --     vim.g.sonokai_style = 'shusia'
+  --     vim.g.sonokai_better_performance = 1
+  --     vim.g.sonokai_transparent_background = 2
+  --     vim.cmd.colorscheme 'sonokai'
+  --   end,
+  -- },
   -- { 'ajmwagar/vim-deus' },
   -- { 'whatyouhide/vim-gotham' },
 }
