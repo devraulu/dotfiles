e902a31ede4cc76a24415b7a3e11c7954d7b1f9c
Raúl Luis <soyrauluis@gmail.com>
2024-09-01 15:28:57 -0400

    Backup config

--- dot_config/nvim/lua/custom/plugins/oil.lua
+++ dot_config/nvim/lua/custom/plugins/oil.lua
@@ -1,7 +1,9 @@
+vim.keymap.set('n', '<leader>fe', ':Oil<CR>', { desc = '[F]ile [e]xplorer', silent = true })
+
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = { view_options = { show_hidden = false } },
   -- Optional dependencies
   -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
 }
--- dot_config/nvim/lua/kickstart/plugins/executable_debug.lua
+++ dot_config/nvim/lua/kickstart/plugins/executable_debug.lua
@@ -1,52 +1,29 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
-
-    -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
   },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_setup = true,
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
       handlers = {},
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
         -- 'javascript',
         -- 'go',
         -- 'typescript',
         -- 'rust',
+        'codelldb',
         'delve',
       },
     }
 
-    -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
@@ -55,6 +32,10 @@ return {
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
+
+    vim.keymap.set('n', '<F4>', dap.terminate, { desc = 'Debug: Reset' })
+    vim.keymap.set('n', '<F6>', dap.run_last, { desc = 'Debug: Run Last' })
+    vim.keymap.set('n', '<F6>', "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = 'Debug: Testables' })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
@@ -87,7 +68,7 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup()
+    -- require('dap-go').setup()
 
     dap.adapters.gdb = {
       type = 'executable',
@@ -109,7 +90,7 @@ return {
     }
 
     dap.configurations.cpp = dap.configurations.c
-    dap.configurations.rust = dap.configurations.c
+    -- dap.configurations.rust = dap.configurations.c
 
     dap.adapters.firefox = {
       type = 'executable',
